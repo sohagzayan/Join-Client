ce86cf4c85bd81b8e6d8ccdc08be35f457402fa0
nazmul162001 <nazmulprime162001@gmail.com>
2024-10-24 12:44:41 +0600

    fixed dependency error and re-solve conflict

---
+++ app/(root)/post-job/page.tsx
@@ -0,0 +1,11 @@
+import JobPost from './components/JobPost';
+
+const page = () => {
+  return (
+    <section className="container">
+      <JobPost />
+    </section>
+  );
+};
+
+export default page;
