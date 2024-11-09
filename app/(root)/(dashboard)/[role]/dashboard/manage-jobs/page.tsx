fc0d6549e31f58a336a5d5d72bcc499b72837ad9
sohagzayan <gdsohag360@gmail.com>
2024-11-09 15:23:20 +0600

    add quick template side bar in right side

--- app/(root)/(dashboard)/[role]/dashboard/manage-jobs/page.tsx
+++ app/(root)/(dashboard)/[role]/dashboard/manage-jobs/page.tsx
@@ -1,11 +1,5 @@
 'use client';
-import { InputField } from '@/components/common';
 import { Button } from '@/components/ui/button';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover';
 import { initialCandidates } from '@/utils/data';
 import {
   DragDropContext,
@@ -13,9 +7,10 @@ import {
   Droppable,
   DropResult,
 } from '@hello-pangea/dnd';
-import { Bell, ChevronDown, Edit2, Plus, Search, Trash2 } from 'lucide-react';
-import React, { useState } from 'react';
-import { DrawerDemo } from './components/DrawerDemo';
+import { Bell, Edit2, Trash2 } from 'lucide-react';
+import { useState } from 'react';
+import JobSwitchCreateHandle from './components/JobSwitchCreateHandle';
+import { QuickTemplate } from './components/QuickTemplate';
 
 // Types
 type Skill = string;
@@ -74,23 +69,9 @@ const initialColumns: { [key in JobStatus]: Column } = {
 };
 
 export default function Job_manage() {
-  const [darkMode, setDarkMode] = useState(false);
   const [candidates, setCandidates] = useState(initialCandidates);
   const [columns, setColumns] = useState(initialColumns);
   const [selectedJob, setSelectedJob] = useState('Senior Java Developer');
-
-  const [currentCompany, setCurrentCompany] =
-    React.useState('TechRecruit Inc.');
-  const [searchTerm, setSearchTerm] = React.useState('');
-  const [companies, setCompanies] = React.useState([
-    'TechRecruit Inc.',
-    'Global Staffing Solutions',
-    'Talent Finders Ltd.',
-  ]);
-
-  const filteredCompanies = companies.filter((company) =>
-    company.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
 
   const onDragEnd = (result: DropResult) => {
     const { source, destination } = result;
@@ -127,95 +108,14 @@ export default function Job_manage() {
     });
   };
 
-  const Sidebar = React.memo(() => (
-    <div className="flex items-center space-x-5 rounded-lg bg-[rgba(255,255,255,0.05)] px-4 py-2 shadow">
-      <div className=" ">
-        <div className="flex items-center space-x-2">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant="ghost"
-                className="px-0 py-2 font-semibold text-white"
-              >
-                {currentCompany}
-                <ChevronDown className="ml-1 h-4 w-4 text-gray-500" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="ml-16 w-64 border-2 border-[rgba(255,255,255,0.1)] bg-[#181C3B] p-0 text-white">
-              <div className="p-2">
-                <div className="relative mb-2">
-                  <InputField
-                    type="search"
-                    icon={Search}
-                    placeholder="Find company..."
-                    // value={searchTerm}
-                    // onChange={(e: any) => setSearchTerm(e.target.value)}
-                    className="rounded-md border-2 border-[rgba(255,255,255,0.1)] bg-themeDark outline-none"
-                  />
-                </div>
-                <div className="max-h-48 overflow-auto">
-                  {filteredCompanies.map((company) => (
-                    <Button
-                      key={company}
-                      variant="ghost"
-                      className="w-full justify-between"
-                      onClick={() => {
-                        setCurrentCompany(company);
-                        setSearchTerm('');
-                      }}
-                    >
-                      {company}
-                      {company === currentCompany && (
-                        <svg
-                          className="text-primary h-4 w-4"
-                          fill="none"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M5 13l4 4L19 7"
-                          />
-                        </svg>
-                      )}
-                    </Button>
-                  ))}
-                </div>
-                <Button variant="ghost" className="mt-2 w-full justify-start">
-                  <Plus className="mr-2 h-4 w-4" />
-                  Create New Job
-                </Button>
-              </div>
-            </PopoverContent>
-          </Popover>
-          <span className="rounded border border-[rgba(0,123,255,0.2)] bg-[rgba(0,123,255,0.1)] px-2 py-0.5 text-xs text-theme1">
-            Free
-          </span>
-        </div>
-      </div>
-      <Button
-        size="icon"
-        variant="outline"
-        className="border-transparent bg-theme1 text-white"
-      >
-        <Plus className="h-4 w-4" />
-      </Button>
-    </div>
-  ));
-
-  Sidebar.displayName = 'Sidebar';
-
   return (
     <div className={`min-h-screen`}>
       <div className="mx-auto p-4">
-        <DrawerDemo />
         <div className="mb-4 flex items-center justify-between">
           <div className="hidden lg:block">
-            <Sidebar />
+            <JobSwitchCreateHandle />
           </div>
+          <QuickTemplate />
         </div>
         <div className="grid">
           <div
